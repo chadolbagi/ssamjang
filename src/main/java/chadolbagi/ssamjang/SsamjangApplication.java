08b2b9132937a44ecb86c1837a2bbc06e926ece7
Seo jinhyeong <e6ipi1@gmail.com>
2020-06-01 15:38:58 +0900

    chore: Spring Boot 프로젝트 세팅

---
+++ src/main/java/chadolbagi/ssamjang/SsamjangApplication.java
@@ -0,0 +1,13 @@
+package chadolbagi.ssamjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsamjangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsamjangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chadolbagi/ssamjang/SsamjangApplicationTests.java
@@ -0,0 +1,13 @@
+package chadolbagi.ssamjang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsamjangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
